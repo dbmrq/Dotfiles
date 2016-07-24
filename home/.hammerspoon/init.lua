a684cbaad70d0f3daecb54f7c943a050750c1d2f
Daniel M <danielbmarques@gmail.com>
2016-07-24 17:16:44 -0300

    Improve window positioning

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,141 +1,6 @@
-rightPosTracker = 0
-leftPosTracker = 0
-topPosTracker = 0
-bottomPosTracker = 0
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    if leftPosTracker == 0 then
-        f.x = max.x + 20
-        f.y = max.y + 20
-        f.w = max.w / 2 - 30
-        f.h = max.h - 40
-        win:setFrame(f)
-        leftPosTracker = 1
-    elseif leftPosTracker == 1 then
-        f.x = max.x + 20
-        f.y = max.y + 20
-        f.w = max.w / 1.5 - 30
-        f.h = max.h - 40
-        win:setFrame(f)
-        leftPosTracker = 2
-    else
-        f.x = max.x + 20
-        f.y = max.y + 20
-        f.w = max.w / 3 - 30
-        f.h = max.h - 40
-        win:setFrame(f)
-        leftPosTracker = 0
-    end
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    if rightPosTracker == 0 then
-        f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + 20
-        f.w = max.w / 2 - 30
-        f.h = max.h - 40
-        win:setFrame(f)
-        rightPosTracker = 1
-    elseif rightPosTracker == 1 then
-        f.x = max.x + (max.w / 1.5) + 10
-        f.y = max.y + 20
-        f.w = max.w / 3 - 30
-        f.h = max.h - 40
-        win:setFrame(f)
-        rightPosTracker = 2
-    else
-        f.x = max.x + (max.w / 3) + 10
-        f.y = max.y + 20
-        f.w = max.w / 1.5 - 30
-        f.h = max.h - 40
-        win:setFrame(f)
-        rightPosTracker = 0
-    end
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    if topPosTracker == 0 then
-        -- f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + 20
-        -- f.w = max.w / 2 - 30
-        f.h = max.h / 2 - 30
-        win:setFrame(f)
-        topPosTracker = 1
-    elseif topPosTracker == 1 then
-        -- f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + 20
-        -- f.w = max.w / 2 - 30
-        f.h = max.h / 3 - 30
-        win:setFrame(f)
-        topPosTracker = 2
-    else
-        -- f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + 20
-        -- f.w = max.w / 2 - 30
-        f.h = max.h / 1.5 - 30
-        win:setFrame(f)
-        topPosTracker = 0
-    end
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    if bottomPosTracker == 0 then
-        -- f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + (max.h / 2) + 10
-        -- f.w = max.w / 2 - 30
-        f.h = max.h / 2 - 30
-        win:setFrame(f)
-        bottomPosTracker = 1
-    elseif bottomPosTracker == 1 then
-        -- f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + (max.h / 1.5) + 10
-        -- f.w = max.w / 2 - 30
-        f.h = max.h / 3 - 30
-        win:setFrame(f)
-        bottomPosTracker = 2
-    else
-        -- f.x = max.x + (max.w / 2) + 10
-        f.y = max.y + (max.h / 3) + 10
-        -- f.w = max.w / 2 - 30
-        f.h = max.h / 1.5 - 30
-        win:setFrame(f)
-        bottomPosTracker = 0
-    end
-end)
-
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x + 20
-    f.y = max.y + 20
-    f.w = max.w - 40
-    f.h = max.h - 40
-    win:setFrame(f)
-end)
-
+---------------------
+--  Reload config  --
+---------------------
 
 function reloadConfig(files)
     doReload = false
@@ -148,6 +13,212 @@ function reloadConfig(files)
         hs.reload()
     end
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+hs.pathwatcher.new(os.getenv("HOME") ..
+    "/.hammerspoon/", reloadConfig):start()
+hs.pathwatcher.new(os.getenv("HOME") ..
+    ".homesick/repos/dotfiles/home/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
+
+
+--------------------------
+--  Window positioning  --
+--------------------------
+
+local hotkey = require "hs.hotkey"
+local super = {"ctrl", "alt", "cmd"}
+
+local grid = require "hs.grid"
+
+grid.setMargins('20,20')
+grid.setGrid('4x4')
+
+
+local function snapToBottom(win, cell, screen)
+    local newCell =
+        hs.geometry(cell.x, grid.GRIDHEIGHT - cell.h, cell.w, cell.h)
+    grid.set(win, newCell, screen)
+end
+
+local function snapToTop(win, cell, screen)
+    local newCell =
+        hs.geometry(cell.x, 0, cell.w, cell.h)
+    grid.set(win, newCell, screen)
+end
+
+local function snapToLeft(win, cell, screen)
+    local newCell =
+        hs.geometry(0, cell.y, cell.w, cell.h)
+    grid.set(win, newCell, screen)
+end
+
+local function snapToRight(win, cell, screen)
+    local newCell =
+        hs.geometry(grid.GRIDWIDTH - cell.w, cell.y, cell.w, cell.h)
+    grid.set(win, newCell, screen)
+end
+
+local function compensateMargins()
+    -- Compensate for the double margin between windows
+    local win = hs.window.focusedWindow()
+    local cell = grid.get(win)
+    local frame = win:frame()
+    if cell.h < grid.GRIDHEIGHT and cell.h % 1 == 0 then
+        if cell.y == 0 then
+            frame.h = frame.h + grid.MARGINX / 2
+            win:setFrame(frame)
+        else
+            frame.h = frame.h + grid.MARGINY / 2
+            frame.y = frame.y - grid.MARGINY / 2
+            win:setFrame(frame)
+        end
+    end
+    if cell.w < grid.GRIDWIDTH and cell.w % 1 == 0 then
+        if cell.x == 0 then
+            frame.w = frame.w + grid.MARGINY / 2
+            win:setFrame(frame)
+        else
+            frame.w = frame.w + grid.MARGINY / 2
+            frame.x = frame.x - grid.MARGINY / 2
+            win:setFrame(frame)
+        end
+    end
+end
+
+
+hotkey.bind(super, ';', grid.maximizeWindow)
+
+hs.hotkey.bind(super, 'Left', function()
+    grid.pushWindowLeft()
+    compensateMargins()
+end)
+
+hs.hotkey.bind(super, 'Down', function()
+    grid.pushWindowDown()
+    compensateMargins()
+end)
+hs.hotkey.bind(super, 'Up', function()
+    grid.pushWindowUp()
+    compensateMargins()
+end)
+hs.hotkey.bind(super, 'Right', function()
+    grid.pushWindowRight()
+    compensateMargins()
+end)
+
+hotkey.bind(super, 'J', function()
+    local win = hs.window.focusedWindow()
+    local cell = grid.get(win)
+    local screen = win:screen()
+    if cell.y < grid.GRIDHEIGHT - cell.h and cell.h % 1 == 0 then
+        snapToBottom(win, cell, screen)
+        compensateMargins()
+        return
+    elseif cell.h % 1 ~= 0 then
+        snapToBottom(win, cell, screen)
+        grid.snap(win)
+        compensateMargins()
+        return
+    end
+    if cell.h <= 1 then
+        growing = true
+    elseif cell.h >= grid.GRIDHEIGHT then
+        growing = false
+    end
+    if growing then
+        grid.resizeWindowTaller()
+    else
+        grid.resizeWindowShorter()
+    end
+    local cell = grid.get(win)
+    snapToBottom(win, cell, screen)
+    compensateMargins()
+end)
+
+hotkey.bind(super, 'K', function()
+    local win = hs.window.focusedWindow()
+    local cell = grid.get(win)
+    local screen = win:screen()
+    if cell.y > 0 and cell.h % 1 == 0 then
+        snapToTop(win, cell, screen)
+        compensateMargins()
+        return
+    elseif cell.h % 1 ~= 0 then
+        snapToTop(win, cell, screen)
+        grid.snap(win)
+        compensateMargins()
+        return
+    end
+    if cell.h <= 1 then
+        growing = true
+    elseif cell.h >= grid.GRIDHEIGHT then
+        growing = false
+    end
+    if growing then
+        grid.resizeWindowTaller()
+    else
+        grid.resizeWindowShorter()
+    end
+    local cell = grid.get(win)
+    snapToTop(win, cell, screen)
+    compensateMargins()
+end)
+
+hotkey.bind(super, 'H', function()
+    local win = hs.window.focusedWindow()
+    local cell = grid.get(win)
+    local screen = win:screen()
+    if cell.x > 0 and cell.w % 1 == 0 then
+        snapToLeft(win, cell, screen)
+        compensateMargins()
+        return
+    elseif cell.w % 1 ~= 0 then
+        snapToLeft(win, cell, screen)
+        grid.snap(win)
+        compensateMargins()
+        return
+    end
+    if cell.w <= 1 then
+        growing = true
+    elseif cell.w >= grid.GRIDWIDTH then
+        growing = false
+    end
+    if growing then
+        grid.resizeWindowWider()
+    else
+        grid.resizeWindowThinner()
+    end
+    local cell = grid.get(win)
+    snapToLeft(win, cell, screen)
+    compensateMargins()
+end)
+
+hotkey.bind(super, 'L', function()
+    local win = hs.window.focusedWindow()
+    local cell = grid.get(win)
+    local screen = win:screen()
+    if cell.x < grid.GRIDWIDTH - cell.w and cell.w % 1 == 0 then
+        snapToRight(win, cell, screen)
+        compensateMargins()
+        return
+    elseif cell.w % 1 ~= 0 then
+        snapToRight(win, cell, screen)
+        grid.snap(win)
+        compensateMargins()
+        return
+    end
+    if cell.w <= 1 then
+        growing = true
+    elseif cell.w >= grid.GRIDWIDTH then
+        growing = false
+    end
+    if growing then
+        grid.resizeWindowWider()
+    else
+        grid.resizeWindowThinner()
+    end
+    local cell = grid.get(win)
+    snapToRight(win, cell, screen)
+    compensateMargins()
+end)
 
