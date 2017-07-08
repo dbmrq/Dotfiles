ff75ec68db9bdde8d4a7d1c73032b2df26d8a8f9
Daniel M <danielbmarques@gmail.com>
2017-07-08 18:48:12 -0300

    Improve .bootstrap.sh

--- home/.bootstrap.sh
+++ home/.bootstrap.sh
@@ -10,6 +10,12 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Software update {{{1
+
+sudo softwareupdate -i -a
+
+# }}}1
+
 # Preferences {{{1
 
 # inspired by https://mths.be/macos
@@ -84,6 +90,9 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 # Disable the crash reporter
 defaults write com.apple.CrashReporter DialogType -string "none"
 
+# Wipe all (default) app icons from the Dock
+defaults write com.apple.dock persistent-apps -array
+
 # }}}2
 
 # }}}1
@@ -92,38 +101,7 @@ defaults write com.apple.CrashReporter DialogType -string "none"
 
 echo "Installing xcode-select..."
 
-if ! xcode-select --print-path &> /dev/null; then
-
-    # Prompt user to install the XCode Command Line Tools
-    xcode-select --install &> /dev/null
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Wait until the XCode Command Line Tools are installed
-    until xcode-select --print-path &> /dev/null; do
-        sleep 5
-    done
-
-    print_result $? 'Install XCode Command Line Tools'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Point the `xcode-select` developer directory to
-    # the appropriate directory from within `Xcode.app`
-    # https://github.com/alrra/dotfiles/issues/13
-
-    sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-    print_result $? 'Make "xcode-select" developer directory point to Xcode'
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Prompt user to agree to the terms of the Xcode license
-    # https://github.com/alrra/dotfiles/issues/10
-
-    sudo xcodebuild -license
-    print_result $? 'Agree with the XCode Command Line Tools licence'
-
-fi
+xcode-select --install
 
 # }}}1
 
@@ -133,26 +111,30 @@ echo "Installing Homebrew and formulae..."
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew tap caskroom/cask
-
-brew install zsh zsh-completions
-brew install bash
-
+brew install git
 brew install lua
 brew install ruby
 brew install luajit
 brew install cscope
-
+brew install carthage
+brew install trash
+brew install fasd
+brew install zsh
+brew install curl --with-openssl
 brew install vim --with-override-system-vi --with-python --with-cscope --with-lua --with-luajit
 
-brew install git
-
-brew install trash
-
+brew tap caskroom/cask
+brew cask install java
 brew cask install hammerspoon
-
 brew cask install google-chrome
 brew cask install firefox
+brew cask install transmission
+brew cask install appcleaner
+brew cask install the-unarchiver
+brew cask install calibre
+brew cask install vlc
+
+brew linkapps
 
 brew cleanup
 
@@ -162,9 +144,7 @@ brew cleanup
 
 echo "Installing Prezto..."
 
-git clone https://github.com/sorin-ionescu/prezto.git ~/Desktop/.zprezto
-rsync -a ~/Desktop/.zprezto ~/
-trash ~/Desktop/.zprezto
+git clone https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 
 # }}}1
 
@@ -179,11 +159,21 @@ homesick rc dotfiles
 
 # }}}1
 
+# Change Shell {{{1
+
+sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
+
+# }}}1
+
 # Terminal colorscheme {{{1
+
+echo "Adding Terminal color schemes..."
 
 open /Users/daniel/.homesick/repos/dotfiles/home/Library/Colors/Solarized\ Dark.terminal
 
 open /Users/daniel/.homesick/repos/dotfiles/home/Library/Colors/Solarized\ Light.terminal
 
 # }}}1
+
+echo "Remember to check Homebrew's results for aditional instructions!"
 
