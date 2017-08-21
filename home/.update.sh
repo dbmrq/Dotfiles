ae6d1f5ea3fa2612a87f83b8b566a6c3aa166ac7
Daniel M <danielbmarques@gmail.com>
2017-08-21 00:35:34 -0300

    Fix MacVim icon and alias conditionally

--- home/.update.sh
+++ home/.update.sh
@@ -6,7 +6,8 @@ echo "---"
 # Software update {{{1
 
 echo ""
-echo "`date`: Running softwareupdate"
+echo "`date`: Running software update"
+echo ""
 softwareupdate --install --all
 
 # }}}1
@@ -15,44 +16,54 @@ softwareupdate --install --all
 
 echo ""
 echo "`date`: Running brew update"
+echo ""
 /usr/local/bin/brew update
 
 echo ""
 echo "`date`: Running brew upgrade"
-/usr/local/bin/brew upgrade
+echo ""
+upgraderesult=$(/usr/local/bin/brew upgrade)
+if [[ "$upgraderesult" ]]; then
+    echo "$upgraderesult"
+fi
 
 echo ""
 echo "`date`: Running brew prune"
+echo ""
 /usr/local/bin/brew prune
 
 echo ""
 echo "`date`: Running brew cleanup"
+echo ""
 /usr/local/bin/brew cleanup
 
 echo ""
 echo "`date`: Running brew cask reinstall for outdated casks"
-/usr/local/bin/brew cask outdated | xargs /usr/local/bin/brew cask reinstall
-
 echo ""
-echo "`date`: Running brew doctor"
-/usr/local/bin/brew doctor
+/usr/local/bin/brew cask outdated | xargs /usr/local/bin/brew cask reinstall
 
 # }}}1
 
 # MacVim dock icon and alias {{{1
 
-echo ""
-echo "`date`: Fixing MacVim Dock icon"
+if echo "$upgraderesult" | grep -q 'MacVim'; then
 
-/usr/local/bin/dockutil --remove 'MacVim' --allhomes
-/usr/local/bin/dockutil --add /usr/local/opt/macvim/MacVim.app --position 2
+    echo ""
+    echo "`date`: Fixing MacVim Dock icon"
+    echo ""
 
-echo ""
-echo "`date`: Fixing MacVim alias"
+    /usr/local/bin/dockutil --remove 'MacVim' --allhomes
+    /usr/local/bin/dockutil --add /usr/local/opt/macvim/MacVim.app --position 2
 
-/usr/local/bin/trash /Applications/MacVim
+    echo ""
+    echo "`date`: Fixing MacVim alias"
+    echo ""
 
-osascript -e 'tell application "Finder" to make alias file to POSIX file "/usr/local/opt/macvim/MacVim.app" at POSIX file "/Applications/"'
+    /usr/local/bin/trash /Applications/MacVim
+
+    osascript -e 'tell application "Finder" to make alias file to POSIX file "/usr/local/opt/macvim/MacVim.app" at POSIX file "/Applications/"'
+
+fi
 
 # }}}1
 
@@ -60,6 +71,7 @@ osascript -e 'tell application "Finder" to make alias file to POSIX file "/usr/l
 
 echo ""
 echo "`date`: Running mas upgrade"
+echo ""
 /usr/local/bin/mas upgrade
 
 # }}}1
@@ -68,6 +80,7 @@ echo "`date`: Running mas upgrade"
 
 echo ""
 echo "`date`: Running gem update"
+echo ""
 /usr/local/bin/gem update —system
 /usr/local/bin/gem update
 
