efcea6e41b1aceb4ee1f6113c0e6fddf1942307f
Daniel M <danielbmarques@gmail.com>
2018-04-04 02:19:06 -0300

    Simplify cascading function

--- home/.hammerspoon/winman.lua
+++ home/.hammerspoon/winman.lua
@@ -181,29 +181,6 @@ hs.hotkey.bind(super, 'Right', function()-- {{{3
     compensateMargins()
 end)-- }}}3
 
-hs.hotkey.bind(super, ',', function()
-    local windows = hs.window.orderedWindows()
-    local screen = windows[1]:screen():frame()
-    local nOfSpaces = #windows > 1 and #windows - 1 or 1
-
-    local xMargin = screen.w / 10 -- unused horizontal margin
-    local yMargin = 20            -- unused vertical margin
-    local spacing = 40            -- the visible margin for each window
-
-    local wholeCascadeSize = nOfSpaces * spacing
-
-    for i, win in ipairs(windows) do
-        local offset = (i - 1) * (wholeCascadeSize / nOfSpaces)
-        local rect = {
-            x = xMargin + offset,
-            y = screen.y + yMargin + offset,
-            w = screen.w - 2 * xMargin - wholeCascadeSize,
-            h = screen.h - 2 * yMargin - wholeCascadeSize,
-        }
-        win:setFrame(rect)
-    end
-end)
-
 -- }}}2
 
 -- Resize windows {{{2
@@ -377,6 +354,31 @@ hs.hotkey.bind(super, '.', function()
         local window = windows[i]
         grid.snap(window)
         compensateMargins(window)
+    end
+end)
+
+-- }}}2
+
+-- Cascade {{{2
+
+hs.hotkey.bind(super, ',', function()
+    local windows = hs.window.orderedWindows()
+    local screen = windows[1]:screen():frame()
+    local nOfSpaces = #windows > 1 and #windows - 1 or 1
+
+    local xMargin = screen.w / 10 -- unused horizontal margin
+    local yMargin = 20            -- unused vertical margin
+    local spacing = 40            -- the visible margin for each window
+
+    for i, win in ipairs(windows) do
+        local offset = (i - 1) * spacing
+        local rect = {
+            x = xMargin + offset,
+            y = screen.y + yMargin + offset,
+            w = screen.w - (2 * xMargin) - (nOfSpaces * spacing),
+            h = screen.h - (2 * yMargin) - (nOfSpaces * spacing),
+        }
+        win:setFrame(rect)
     end
 end)
 
