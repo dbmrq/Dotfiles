5062de5877a07d055e949010aec3d9fc17b725bd
Daniel M <danielbmarques@gmail.com>
2022-10-01 21:26:36 -0300

    Have user pick directory

--- Bootstrap/tlmgr.sh
+++ Bootstrap/tlmgr.sh
@@ -12,18 +12,25 @@ sudo tlmgr install scheme-medium collection-humanities collection-langgreek \
 collection-langother collection-latexextra collection-pictures logreq \
 biblatex biber
 
-mkdir -pv ~/Code/LaTeX/Bibliography
-mkdir -pv ~/Code/LaTeX/Classes
-mkdir -pv ~/Code/LaTeX/Packages
+read -p "Choose a directory for your TeX supporting files" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    osascript -e "tell application \"Terminal\" to do script \"source ${PWD}/mas.sh\""
+fi
+
+mkdir -pv ${REPLY%/}/Classes
+mkdir -pv ${REPLY%/}/Packages
+mkdir -pv ${REPLY%/}/Bibliography
 mkdir -pv ~/Library/texmf/tex/latex
 mkdir -pv ~/Library/texmf/bibtex
 
-git clone https://github.com/dbmrq/tex-dbmrq.git ~/Code/LaTeX/Classes/dbmrq
-git clone https://github.com/abntex/biblatex-abnt.git ~/Code/LaTeX/Packages/biblatex-abnt
+git clone https://github.com/dbmrq/tex-dbmrq.git ${REPLY%/}/Classes/dbmrq
+git clone https://github.com/abntex/biblatex-abnt.git ${REPLY%/}/Packages/biblatex-abnt
 
-ln -s ~/Code/LaTeX/Classes ~/Library/texmf/tex/latex/classes
-ln -s ~/Code/LaTeX/Packages ~/Library/texmf/tex/latex/packages
-ln -s ~/Code/LaTeX/Bibliography ~/Library/texmf/bibtex/bib
+ln -s ${REPLY%/}/Classes ~/Library/texmf/tex/latex/classes
+ln -s ${REPLY%/}/Packages ~/Library/texmf/tex/latex/packages
+ln -s ${REPLY%/}/Bibliography ~/Library/texmf/bibtex/bib
 
 echo ""
 echo "Done."
