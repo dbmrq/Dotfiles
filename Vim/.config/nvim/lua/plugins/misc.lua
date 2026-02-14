fcc6acb714ffc314035a143d7cf8c2f200f062ad
Daniel Marques <danielbmarques@gmail.com>
2026-02-14 11:49:38 -0300

    Remove vim-dojo plugin

--- Vim/.config/nvim/lua/plugins/misc.lua
+++ Vim/.config/nvim/lua/plugins/misc.lua
@@ -3,13 +3,4 @@ return {
         "karb94/neoscroll.nvim",
         opts = {},
     },
-    {
-        "akhutsishvili/vim-dojo.nvim",
-        cmd = { "VimDojo", "VimDojoStats" },
-        keys = {
-            { "<leader>vd", "<cmd>VimDojo<cr>", desc = "Vim Dojo" },
-            { "<leader>vs", "<cmd>VimDojoStats<cr>", desc = "Vim Dojo Stats" },
-        },
-        opts = {},
-    },
 }
